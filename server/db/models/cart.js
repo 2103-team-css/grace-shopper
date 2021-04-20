348d6c09fc2531aaeabc2a88495f960561a91609
Chris Gil Martins <cgilmartins@gmail.com>
2021-04-20 12:09:15 -0400

    BE: Define cart model and associations

---
+++ server/db/models/cart.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Cart = db.define('cart', {
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+    },
+  },
+  total: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      min: 0,
+    },
+  },
+});
+
+module.exports = Cart;
