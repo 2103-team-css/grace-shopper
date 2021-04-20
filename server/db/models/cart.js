8652667dac612c200095689f756eb7f2d581c781
Chris Gil Martins <cgilmartins@gmail.com>
2021-04-20 12:16:40 -0400

    BE: Added id to carts

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -2,6 +2,12 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Cart = db.define('cart', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
   quantity: {
     type: Sequelize.INTEGER,
     allowNull: false,
