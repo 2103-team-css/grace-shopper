cfe7bf47b127cdf613744824c923e8804f16275a
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2021-04-21 08:13:16 -0700

    added fixed redux store, added map for all prod viewpage, wrote get route for products,

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchProducts } from '../store/products';
 
-// const AllProducts = (props) => {
-//   console.log(props);
-//   return (
-//     <div className='all-products-container'>Container for All Products</div>
-//   );
-// };
+const AllProducts = (props) => {
+  useEffect(() => {
+    props.getProducts();
+  }, []);
+  const { products } = props;
+  console.log(products);
 
-export class AllProducts extends Component {
-  componentDidMount() {
-    this.props.getProducts();
-  }
+  //l 23 filt products before map
+  //arr of filt prod
+  //l28 use that instead of using full prod array
+  //filt conneted to state
+  //loc state holds filt
 
-  render() {
-    console.log(this.props);
-    return <div>hello world</div>;
-  }
-}
+  return (
+    <div className='all-products-container'>
+      <div className='filter-div'>
+        <label for='products'>Filter by product category</label>
+        <select name='instruments' id='instruments'>
+          <option value='percussion'>percussion</option>
+          <option value='strings'>strings</option>
+          <option value='keys'>keys</option>
+          <option value='accessories'>accessories</option>
+        </select>
+        {products.map((product) => {
+          return (
+            <div key={product.id} className='product-container'>
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                heigt='150'
+                width='200'
+                className='product-image'
+              />
+              <h3>Name:{product.name}</h3>
+              <p>Price: ${product.price}</p>
+              <p>Add To Cart</p>
+              <p>{product.quantity} Left in Stock</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 const mapState = (state) => {
   return {
