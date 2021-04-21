0adb516dc3b2ac02358ed3a18b1bbaf77fbb72ef
Chiara Wahsono <chiarawahsono@gmail.com>
2021-04-21 15:27:56 -0400

    FE: edit product form

--- client/store/products.js
+++ client/store/products.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 //action types
-const SET_PRODUCTS = 'SET_PRODUCTS';
+export const SET_PRODUCTS = "SET_PRODUCTS";
+export const CREATE_PRODUCT = "CREATE_PRODUCT";
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 //action creator
 const setProducts = (products) => {
@@ -10,13 +12,49 @@ const setProducts = (products) => {
     products,
   };
 };
+export const _createProduct = (product) => {
+  return {
+    type: CREATE_PRODUCT,
+    product,
+  };
+};
+
+export const _updateProduct = (product) => {
+  return {
+    type: UPDATE_PRODUCT,
+    product,
+  };
+};
 
 //thunk
 
 export const fetchProducts = () => {
   return async (dispatch) => {
-    const { data: products } = await axios.get('/api/products');
+    const { data: products } = await axios.get("/api/products");
     dispatch(setProducts(products));
+  };
+};
+
+export const createProduct = (product, history) => {
+  try {
+    return async (dispatch) => {
+      const { data: created } = await axios.post("/api/products", product);
+      dispatch(_createProduct(created));
+      history.push("/");
+    };
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateProduct = (product, history) => {
+  return async (dispatch) => {
+    const { data: updated } = await axios.put(
+      `/api/product/${product.id}`,
+      product
+    );
+    dispatch(_updateProduct(updated));
+    history.push("/");
   };
 };
 
@@ -26,6 +64,12 @@ export default function productsReducer(state = [], action) {
   switch (action.type) {
     case SET_PRODUCTS:
       return action.products;
+    case CREATE_PRODUCT:
+      return [...state, action.product];
+    case UPDATE_PRODUCT:
+      return state.map((product) =>
+        product.id === action.product.id ? action.product : product
+      );
     default:
       return state;
   }
