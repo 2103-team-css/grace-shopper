8adcdb1faee68dbe6209171f64e9feccfc91d092
Chris Gil Martins <cgilmartins@gmail.com>
2021-04-23 11:19:09 -0400

    BE: Converted to super many to many relationships

--- server/db/index.js
+++ server/db/index.js
@@ -7,15 +7,25 @@ const Product = require('./models/product');
 const Cart = require('./models/cart');
 const Order = require('./models/order');
 
+//Define super many to many relationships
 User.belongsToMany(Product, { through: Cart, foreignKey: 'userId' });
 Product.belongsToMany(User, { through: Cart, foreignKey: 'productId' });
+User.hasMany(Cart);
+Cart.belongsTo(User);
+Product.hasMany(Cart);
+Cart.belongsTo(Product);
 
 User.belongsToMany(Product, { through: Order, foreignKey: 'userId' });
 Product.belongsToMany(User, { through: Order, foreignKey: 'productId' });
+User.hasMany(Order);
+Order.belongsTo(User);
+Product.hasMany(Order);
+Order.belongsTo(Product);
 
 module.exports = {
   db,
   User,
   Product,
   Cart,
+  Order,
 };
